e1edece5e31c2065e16105cde7479393e2544f71
ScottSung7 <sungscott@naver.com>
2024-05-21 08:21:52 +0900

    [003-Kafka 도입] Producer 추가

--- api/src/main/java/net/scottcommerce/api/service/ApplyService.java
+++ api/src/main/java/net/scottcommerce/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package net.scottcommerce.api.service;
 
 import jakarta.transaction.Transactional;
 import net.scottcommerce.api.domain.Coupon;
+import net.scottcommerce.api.producer.CouponCreateProducer;
 import net.scottcommerce.api.repository.CouponCountRepository;
 import net.scottcommerce.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,12 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    private final CouponCreateProducer couponCreateProducer;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
     public void apply(Long userId) {
         Long count = couponCountRepository.increment();
@@ -24,6 +28,7 @@ public class ApplyService {
             return;
         }
 
+        //couponCreateProducer.create(userId);
        couponRepository.save(new Coupon(userId));
 
     }
